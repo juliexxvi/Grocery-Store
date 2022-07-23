7bf3e1fb908b21e520f9c6ed546fb3e04b2e4694
Julie Nguyen <julie.hyen@gmail.com>
2022-07-23 22:44:47 +1000

    Main page and pagination

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from unittest import result
+from flask import Flask, make_response, request, redirect, render_template, session
+import psycopg2
+# from hash import hash, check
+from functools import wraps
 
 app = Flask(__name__)
+app.secret_key = b'b0af2307-0335-442c-acb1-8ac4639ab9ec'
+conn = psycopg2.connect("dbname=grocery_store")
+cur = conn.cursor()
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello, world!</h1>'
+    page = request.args.get('page')  # http://127.0.0.1:5000/?page=1
+    if page is None:
+        page = 1
+    cur.execute("""SELECT count(*) FROM products""")
+    results = cur.fetchall()
+    page_count = int(results[0][0] / 36)
+    offset = (int(page) - 1) * 36
+    cur.execute("""SELECT * FROM products LIMIT 36 OFFSET %s""", [offset])
+    results = cur.fetchall()
+    products = []
+    for row in results:
+        products.append({
+            "id": row[0],
+            "name": row[1],
+            "image_url": row[2],
+            "price": row[3]/100,
+            "brand": row[4],
+            "store": row[5],
+            "code": row[6],
+            "cup_price": row[7],
+            "package_size": row[8]
+        })
+    return render_template(('index.html'), products=products, page_count=page_count, page=page)
 
 
 if __name__ == "__main__":
---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+SELECT 'CREATE DATABASE grocery_store' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'grocery_store') \gexec
+
+\c grocery_store
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  image_url text,
+  price_in_cents integer NOT NULL,
+  brand text,
+  store text NOT NULL,
+  code text NOT NULL,
+  cup_price text,
+  package_size text
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+  id serial PRIMARY KEY,
+  customer_name text NOT NULL,
+  customer_address text NOT NULL,
+  total_amount integer NOT NULL
+);
+
+DROP TABLE IF EXISTS order_details;
+CREATE TABLE order_details (
+  id serial PRIMARY KEY,
+  order_id integer REFERENCES orders(id),
+  product_id integer REFERENCES products(id),
+  quantity integer NOT NULL,
+  unit_price_in_cents integer NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+  id serial PRIMARY KEY, 
+  email text NOT NULL, 
+  name text NOT NULL,
+  password_hash text NOT NULL,
+  role text NOT NULL
+);
+
+
+
