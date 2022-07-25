5938507737ec731ed2eb7c8b7cc12a0af4f66e2d
Julie Nguyen <julie.hyen@gmail.com>
2022-07-25 21:46:41 +1000

    Check-out and remove from cart

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from itertools import product
 from unittest import result
 from flask import Flask, make_response, request, redirect, render_template, session
 import psycopg2
@@ -27,7 +28,7 @@ def index():
             "id": row[0],
             "name": row[1],
             "image_url": row[2],
-            "price": row[3]/100,
+            "price": row[3],
             "brand": row[4],
             "store": row[5],
             "code": row[6],
@@ -41,14 +42,51 @@ def index():
 @app.route('/check-out', methods=['GET', 'POST'])
 def check_out():
     if request.method == 'GET':
-        return render_template('check-out.html')
+        cart_items = session.get('cart')
+        cart_products = []
+        total_cart_amount = 0
+        for cart_item in cart_items:
+            cart_item_id = cart_item['id']
+            cart_item_quantity = cart_item['quantity']
+            cur.execute('SELECT * FROM products WHERE id=%s', [cart_item_id])
+            row = cur.fetchall()[0]
+            cart_products.append({
+                "id": row[0],
+                "name": row[1],
+                "image_url": row[2],
+                "price": row[3],
+                "brand": row[4],
+                "store": row[5],
+                "code": row[6],
+                "cup_price": row[7],
+                "package_size": row[8],
+                "quantity": cart_item_quantity,
+                "total_amount": cart_item_quantity*row[3]
+            })
+            total_cart_amount += cart_item_quantity*row[3]
+        return render_template('check-out.html', cart_products=cart_products, total_cart_amount=total_cart_amount)
     # if request.method == 'POST':
+
+
+@app.route('/delete-item-action', methods=['POST'])
+def delete_item_action():
+    product_id = int(request.args.get('product-id'))
+    cart_items = session.get('cart')
+    total_quantity = session.get('total_quantity')
+    for i in range(0, len(cart_items)):
+        if product_id == cart_items[i]['id']:
+            total_quantity -= cart_items[i]['quantity']
+            cart_items.pop(i)
+            break
+    session['cart'] = cart_items
+    session['total_quantity'] = total_quantity
+    return redirect('/check-out')
 
 
 @app.route('/add-to-cart-action', methods=['POST'])
 def add_to_cart_action():
     current_page = request.args.get('page')
-    product_id = request.args.get('product-id')
+    product_id = int(request.args.get('product-id'))
     total_quantity = session.get('total_quantity')
     quantity = int(request.form.get('quantity'))
     if total_quantity is None:
@@ -70,7 +108,7 @@ def add_to_cart_action():
                 'quantity': quantity
             })
         session['cart'] = arr
-    print(session.get('cart'))
+    # print(session.get('cart'))
     return redirect(f'/?page={current_page}')
 
 
